fb8f0329114c13e341de508b1cef7ee87b54a26d
kumard3 <kumardeepanshu157200@gmail.com>
2022-04-27 18:40:24 +0530

    first commit

---
+++ pages/api/books.ts
@@ -0,0 +1,72 @@
+// // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  message: string
+  success: boolean
+
+}
+
+type Response = {
+  success: any
+  error: string
+}
+
+// export default function handler(
+// req: NextApiRequest,
+// res: NextApiResponse<Data>
+// ) {
+//   res.status(200).json({ name: 'John Doe' })
+// }
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+
+export default async function handler(req: NextApiRequest,
+  res: NextApiResponse<Data>) {
+  if (req.method === 'POST') {
+    //@ts-ignore
+    return await addBook(req, res)
+  } else if (req.method === 'GET') {
+    //@ts-ignore
+    return await readBooks(req, res)
+  }
+  else {
+    return res.status(405).json({ message: 'Method not allowed', success: false });
+  }
+}
+
+async function addBook(req: NextApiRequest, res: NextApiResponse<Response>) {
+  const body = req.body;
+  try {
+    //@ts-ignore
+    const newEntry = await prisma.bookSuggestion.create({
+      data: {
+        bookeTitle: body.bookTitle,
+        bookeAuthor: body.bookAuthor,
+        bookeGenre: body.bookGenre,
+      }
+    });
+    //@ts-ignore
+    return res.status(200).json(newEntry, { success: true });
+  } catch (error) {
+    console.error("Request error", error);
+    res.status(500).json({ error: "Error creating question", success: false });
+  }
+}
+
+
+async function readBooks(req: NextApiRequest,
+  res: NextApiResponse) {
+  try {
+    const books = await prisma.bookSuggestion.findMany();
+    //@ts-ignore
+    return res.status(200).json(books, { success: true });
+  }
+  catch (error) {
+    console.log(error)
+    return res.status(500).json({ error: "Error reading", success: false });
+  }
+}
---
+++ pages/index.tsx
@@ -0,0 +1,134 @@
+/* eslint-disable @next/next/link-passhref */
+
+import React, { useState, useEffect } from 'react'
+
+export default function Home() {
+  const [bookTitle, setBookTitle] = useState('')
+  const [bookAuthor, setBookAuthor] = useState('')
+  const [bookGenre, setBookGenre] = useState('')
+  const [bookData, setBookData] = useState([])
+  const resetForm = () => {
+    setBookTitle('')
+    setBookAuthor('')
+    setBookGenre('')
+  }
+
+  const GetBooks = async () => {
+    try {
+      const response = await fetch('/api/books', {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+      })
+      setBookData(await response.json())
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const handelSubmit = async (e: any) => {
+    e.preventDefault()
+    const body = { bookTitle, bookAuthor, bookGenre }
+    try {
+      const response = await fetch('/api/books', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body),
+      })
+      if (response.status !== 200) {
+        console.log('something went wrong')
+      } else {
+        resetForm()
+        console.log('success')
+        GetBooks()
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  console.log(bookTitle, bookAuthor, bookGenre)
+  return (
+    <div className="w-full min-h-screen">
+      <div className="flex justify-center bg-black text-white items-center ">
+        <form action="#" method="POST">
+          <div className=" my-6">
+            <label
+              htmlFor="bookTitle"
+              className="block mx-1 text-2xl font-mono"
+            >
+              Book Title
+            </label>
+            <div className="">
+              <input
+                type="text"
+                name="bookTitle"
+                id="bookTitle"
+                onChange={(e) => setBookTitle(e.target.value)}
+                placeholder="Book Title"
+                className="px-6 py-2 text-3xl rounded-xl text-black"
+              />
+            </div>
+          </div>
+          <div className=" my-6">
+            <label
+              htmlFor="bookAuthor"
+              className="block mx-1 text-2xl font-mono"
+            >
+              Book Author
+            </label>
+            <div className="">
+              <input
+                type="text"
+                name="bookAuthor"
+                id="bookAuthor"
+                onChange={(e) => setBookAuthor(e.target.value)}
+                placeholder="Book Author"
+                className="px-6 py-2 text-3xl rounded-xl text-black"
+              />
+            </div>
+          </div>
+          <div className=" my-6">
+            <label
+              htmlFor="bookGenre"
+              className="block mx-1 text-2xl font-mono"
+            >
+              Book Genre
+            </label>
+            <div className="">
+              <input
+                type="text"
+                name="bookGenre"
+                id="bookGenre"
+                onChange={(e) => setBookGenre(e.target.value)}
+                placeholder="Book Genre"
+                className="px-6 py-2 text-3xl rounded-xl text-black"
+              />
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="border-white border px-6 py-2 rounded text-xl"
+            onClick={handelSubmit}
+          >
+            {' '}
+            Submit
+          </button>
+        </form>
+        {/* {bookData && <div>{bookData.map((n) => {})} </div>} */}
+      </div>
+      <div>
+        {bookData?.map(
+          (
+            n: { bookeTitle: string; bookeAuthor: string; bookeGenre: string },
+            index,
+          ) => {
+            return (
+              <div key={index} className="text-white">
+                {n.bookeTitle}
+              </div>
+            )
+          },
+        )}
+      </div>
+    </div>
+  )
+}
